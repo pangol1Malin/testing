079aa211579720523798fdd1d9cc68832ca66325
pangol1Malin <margoulinxyz@gmail.com>
2025-02-17 19:42:34 +0100

    Add app/layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import './globals.css';
+import { Inter } from 'next/font/google';
+import { ThemeProvider } from '@/components/theme-provider';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'Testing',
+  description: 'Documentation technique',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="fr" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
