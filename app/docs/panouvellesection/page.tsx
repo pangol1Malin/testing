6eb47f3fb5130bd2cdad1e2106a389ff24b47c2c
pangol1Malin <margoulinxyz@gmail.com>
2025-02-17 19:42:33 +0100

    Add app/docs/panouvellesection/page.tsx

---
+++ app/docs/panouvellesection/page.tsx
@@ -0,0 +1,115 @@
+
+import Layout from "@/components/Layout";
+import ReactMarkdown from "react-markdown";
+
+export default function Page() {
+  return (
+    <Layout title="PANouvelleSection">
+      <article className="prose dark:prose-invert max-w-none">
+        <h1>PANouvelleSection</h1>
+        <ReactMarkdown>
+          {``}
+        </ReactMarkdown>
+        
+        <h2 id="nouvelle%20sous-section">Nouvelle Sous-section</h2>
+        <ReactMarkdown>
+          {`# Contenu Rédigié
+
+> Citation
+
+# Contenu Rédigié
+
+> Citation
+
+# Contenu Rédigié
+
+> Citation
+
+# Contenu Rédigié
+
+> Citation
+
+# Contenu Rédigié
+
+> Citation
+
+# Contenu Rédigié
+
+> Citation
+
+# Contenu Rédigié
+
+> Citation
+
+# Contenu Rédigié
+
+> Citation
+
+# Contenu Rédigié
+
+> Citation
+
+# Contenu Rédigié
+
+> Citation
+
+# Contenu Rédigié
+
+> Citation
+
+# Contenu Rédigié
+
+> Citation
+
+# Contenu Rédigié
+
+> Citation
+
+# Contenu Rédigié
+
+> Citation
+
+# Contenu Rédigié
+
+> Citation
+
+# Contenu Rédigié
+
+> Citation
+
+# Contenu Rédigié
+
+> Citation
+
+# Contenu Rédigié
+
+> Citation
+
+# Contenu Rédigié
+
+> Citation
+
+# Contenu Rédigié
+
+> Citation
+
+# Contenu Rédigié
+
+> Citation
+
+# Contenu Rédigié
+
+> Citation
+
+# Contenu Rédigié
+
+> Citation
+
+# Contenu Rédigié
+
+> Citation`}
+        </ReactMarkdown>
+      </article>
+    </Layout>
+  );
+}
